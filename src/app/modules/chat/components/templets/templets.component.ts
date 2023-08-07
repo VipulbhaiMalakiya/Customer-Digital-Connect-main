e8872a6cd0a0921a948db3ecd713d38b0104d602
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2023-08-07 16:57:26 +0530

    update templet

--- src/app/modules/chat/components/templets/templets.component.ts
+++ src/app/modules/chat/components/templets/templets.component.ts
@@ -16,6 +16,10 @@ export class TempletsComponent implements OnInit{
   templetsdata:any = [];
   templet:any = {};
   term: any;
+  imageURL: any = '../../../../../assets/images/ceo-template.jpeg';
+  previewUrl: any;
+  uploadFile?:any;
+  fileurl?:any;
   constructor(
     private activeModal: NgbActiveModal,
     private modalService: NgbModal,
@@ -38,6 +42,9 @@ export class TempletsComponent implements OnInit{
          if (data) {
            this.templetsdata = data.data;
            this.templet = this.templetsdata[0];
+           this.fileurl  =  this.templet?.header?.file;
+           console.log(this.fileurl);
+
            this.isProceess = false;
            this.cd.detectChanges();
          }
@@ -50,7 +57,35 @@ export class TempletsComponent implements OnInit{
 
   onView(i:any){
     this.templet = i;
+    this.fileurl  =  i?.header?.file;
+
   }
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    // this.customersMasterForm.get('image').setValue(file);
+    // Image preview
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.previewUrl = reader.result;
+    };
+    reader.readAsDataURL(file);
+
+    let data;
+    if (event.target.files && event.target.files[0])
+      data = event.target.files[0];
+    if (event.target.files[0].name && event.target.files.length > 0) {
+      data = event.target.files[0];
+    }
+    else {
+      data = "null"
+    }
+    this.uploadFile = data;
+    this.fileurl = this.uploadFile?.name
+
+
+  }
+
+
   onCancel() {
     this.activeModal.dismiss();
   }
