03c50841da4fd6c51ca87b827a1b20caaa63cecd
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2023-08-08 16:39:47 +0530

    update

--- src/app/modules/chat/components/templets/templets.component.ts
+++ src/app/modules/chat/components/templets/templets.component.ts
@@ -8,57 +8,52 @@ import { ApiService } from 'src/app/_api/rxjs/api.service';
 @Component({
   selector: 'app-templets',
   templateUrl: './templets.component.html',
-  styleUrls: ['./templets.component.css']
+  styleUrls: ['./templets.component.css'],
 })
-export class TempletsComponent implements OnInit{
+export class TempletsComponent implements OnInit {
   isProceess: boolean = true;
   masterName?: any;
-  templetsdata:any = [];
-  templet:any = {};
+  templetsdata: any = [];
+  templet: any = {};
+  dataArray: any[] = [];
   term: any;
   imageURL: any = '../../../../../assets/images/ceo-template.jpeg';
   previewUrl: any;
-  uploadFile?:any;
-  fileurl?:any;
+  uploadFile?: any;
   constructor(
     private activeModal: NgbActiveModal,
     private modalService: NgbModal,
     private toastr: ToastrService,
     private cd: ChangeDetectorRef,
     private router: Router,
-    private apiService: ApiService,) {
-  }
+    private apiService: ApiService
+  ) {}
 
   ngOnInit(): void {
     this.fatchData();
   }
-  fatchData(){
+  fatchData() {
     this.masterName = '/meta-templates';
     this.apiService
-     .getAll(this.masterName)
-     .pipe(take(1))
-     .subscribe(
-       (data) => {
-         if (data) {
-           this.templetsdata = data.data;
-           this.templet = this.templetsdata[0];
-           this.fileurl  =  this.templet?.header?.file;
-           console.log(this.fileurl);
-
-           this.isProceess = false;
-           this.cd.detectChanges();
-         }
-       },
-       (error) => {
-         this.isProceess = false;
-       }
-     );
+      .getAll(this.masterName)
+      .pipe(take(1))
+      .subscribe(
+        (data) => {
+          if (data) {
+            this.templetsdata = data.data;
+            this.templet = this.templetsdata[0];
+            this.isProceess = false;
+            this.cd.detectChanges();
+          }
+        },
+        (error) => {
+          this.isProceess = false;
+        }
+      );
   }
 
-  onView(i:any){
+  onView(i: any) {
     this.templet = i;
-    this.fileurl  =  i?.header?.file;
-
   }
   onFileChange(event: any) {
     const file = event.target.files[0];
@@ -75,13 +70,10 @@ export class TempletsComponent implements OnInit{
       data = event.target.files[0];
     if (event.target.files[0].name && event.target.files.length > 0) {
       data = event.target.files[0];
-    }
-    else {
-      data = "null"
+    } else {
+      data = 'null';
     }
     this.uploadFile = data;
-    this.fileurl = this.uploadFile?.name
-
 
   }
 
@@ -91,23 +83,40 @@ export class TempletsComponent implements OnInit{
 
   submitForm(formData: any) {
     if (formData) {
-      let data :any =  {
-        templateName : this.templet.templateName,
-        templateBody : {
-          body:this.templet?.body?.body,
-          bodyattribute:["Vipul Malakiya"]
-        },
-        templateHeader:{
-          header:this.templet?.header?.header,
-          headerFileType:this?.templet?.header?.headerFileType,
-          link:this.fileurl
-        }
+      let name = formData.value.input1;
+      let email = formData.value.input2;
+      let password = formData.value.input3
+
+
+      if (name && email && password) {
+        const newData = [name, email, password];
+        this.dataArray.push(newData);
       }
-      this.activeModal.close(data)
+      else if(name && email){
+        const newData = [name, email];
+        this.dataArray.push(newData);
+      }
+      else if(name){
+        const newData = [name];
+        this.dataArray.push(newData);
+      }
+      this.dataArray = [].concat(...this.dataArray);
+
+      let data: any = {
+        templateName: this.templet.templateName,
+        templateBody: {
+          body: this.templet?.body?.body,
+          bodyattribute: this.dataArray,
+        },
+        templateHeader: {
+          header: this.templet?.header?.header,
+          headerFileType: this?.templet?.header?.headerFileType,
+          link: this.templet?.header?.file,
+        },
+      };
+      this.activeModal.close(data);
     } else {
       console.log('Form is invalid.');
     }
   }
-
-
 }
