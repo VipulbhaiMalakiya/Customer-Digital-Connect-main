f499a624366d55448a668264d726184315ab9df8
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2023-09-04 11:21:05 +0530

    inbox update

--- src/app/_layouts/inbox-layout/inbox-layout.component.ts
+++ src/app/_layouts/inbox-layout/inbox-layout.component.ts
@@ -7,13 +7,14 @@ import { NavigationStart, Router } from '@angular/router';
   styleUrls: ['./inbox-layout.component.css'],
 })
 export class InboxLayoutComponent {
-  classToggled = true;
+  classToggled = false;
   currentRoute: any;
   currentUrl: any;
 
   constructor(private router: Router, private cd: ChangeDetectorRef) {
     this.router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
+
         if (event['url'] === '/admin/inbox') {
           this.classToggled = true;
           this.cd.detectChanges();
@@ -29,6 +30,14 @@ export class InboxLayoutComponent {
         }
       }
     });
+
+    if (this.router.url === '/admin/inbox') {
+      this.classToggled = true;
+    } else if (this.router.url === '/inbox') {
+      this.classToggled = true;
+    } else if (this.router.url === '/admin/inbox/:id') {
+      this.classToggled = true;
+    }
   }
 
   openMenu() {
