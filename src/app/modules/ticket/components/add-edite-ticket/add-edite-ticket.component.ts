7a42c4cc3cef2b79fe5b3acab4519ca74a262f92
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2023-09-13 17:59:15 +0530

    update

--- src/app/modules/ticket/components/add-edite-ticket/add-edite-ticket.component.ts
+++ src/app/modules/ticket/components/add-edite-ticket/add-edite-ticket.component.ts
@@ -3,7 +3,10 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ApiService } from 'src/app/_api/rxjs/api.service';
 import { CategoryMasterModel } from 'src/app/_models/category';
-import { MaindefaultPriorityMaster, defaultPriorityMaster } from 'src/app/_models/defaultPriority';
+import {
+  MaindefaultPriorityMaster,
+  defaultPriorityMaster,
+} from 'src/app/_models/defaultPriority';
 import { departmentMasterModel } from 'src/app/_models/department';
 import { issueMasterModel } from 'src/app/_models/issue';
 import { servicetitleMasterModel } from 'src/app/_models/servicetitle';
@@ -13,7 +16,7 @@ import { UserMaster } from 'src/app/_models/user';
 @Component({
   selector: 'app-add-edite-ticket',
   templateUrl: './add-edite-ticket.component.html',
-  styleUrls: ['./add-edite-ticket.component.css']
+  styleUrls: ['./add-edite-ticket.component.css'],
 })
 export class AddEditeTicketComponent {
   private _tickettsMaster: ticketMasterModel | undefined;
@@ -27,11 +30,11 @@ export class AddEditeTicketComponent {
   duser: UserMaster[] = [];
   issueData: issueMasterModel[] = [];
   slaData: MaindefaultPriorityMaster[] = [];
-  uploadFile: any = "";
-  selectedOption:any;
-  selectedOption1:any;
+  uploadFile: any = '';
+  selectedOption: any;
+  selectedOption1: any;
   selectedValue: any;
-  uploadFile1: any = "";
+  uploadFile1: any = '';
   categoryid: any;
   masterName?: any;
   userData: any;
@@ -41,7 +44,7 @@ export class AddEditeTicketComponent {
   chek: any;
 
   get title(): string {
-    return this._tickettsMaster ? "Edit Ticket" : " Add Ticket";
+    return this._tickettsMaster ? 'Edit Ticket' : ' Add Ticket';
   }
   set ticketsMaster(value: ticketMasterModel) {
     this._tickettsMaster = value;
@@ -69,36 +72,38 @@ export class AddEditeTicketComponent {
     private formBuilder: FormBuilder,
     private cd: ChangeDetectorRef,
     private apiService: ApiService
-
   ) {
     this.ticketMasterForm = this.formBuilder.group({
       subCategory: ['', [Validators.required]],
       category: ['', [Validators.required]],
       serviceTitle: ['', [Validators.required]],
-      alternativeContactNo: ['', [
-        Validators.required,
-        Validators.minLength(10),
-        Validators.maxLength(10),
-        Validators.pattern('^[0-9]*$')]],
+      alternativeContactNo: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(10),
+          Validators.pattern('^[0-9]*$'),
+        ],
+      ],
       priority: ['', [Validators.required]],
       issue: ['', [Validators.required]],
       department: ['', [Validators.required]],
       assignedTo: ['', [Validators.required]],
-      shortNotes: ["", [
-        Validators.required]],
+      shortNotes: ['', [Validators.required]],
       status: [true, [Validators.required]],
       createForUser: ['', [Validators.required]],
       file: [''],
       additionalComments: ['', [Validators.required]],
     });
 
-    const d: any = localStorage.getItem("userData");
+    const d: any = localStorage.getItem('userData');
     this.userData = JSON.parse(d);
     this.nrSelect = this.userData.userId;
   }
   ngOnInit(): void {
     this.categoryid = this._tickettsMaster;
-    this.uploadFile = this._tickettsMaster?.fileUrl
+    this.uploadFile = this._tickettsMaster?.fileUrl;
     // this.slaGetData();
     this.fatchData();
     if (this._tickettsMaster) {
@@ -129,61 +134,80 @@ export class AddEditeTicketComponent {
   }
 
   fatchData() {
-    this.masterName = "/category/active"
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.data = data;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+    this.masterName = '/category/active';
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.data = data;
+        this.isProceess = false;
+        this.cd.detectChanges();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
 
   attachmentDownload() {
     this.isProceess = true;
     this.masterName = `/ticket/file/52`;
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
-
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.isProceess = false;
+        this.cd.detectChanges();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
 
   activeIssue() {
-    this.masterName = "/issue/active"
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.issueData = data;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+    this.masterName = '/issue/active';
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.issueData = data;
+        this.isProceess = false;
+        this.cd.detectChanges();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
 
   selectserviceTitle(e: any) {
-    this.masterName = `/servicetitle/servicePriority/${e}`
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.slaData = data;
-      this.selectedOption = this.slaData[0].defaultPriority.id;
+    if (e !== '') {
+      this.masterName = `/servicetitle/servicePriority/${e}`;
+      this.apiService.getAll(this.masterName).subscribe(
+        (data) => {
+          this.slaData = data;
+          this.selectedOption = this.slaData[0].defaultPriority.id;
 
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+          this.isProceess = false;
+          this.cd.detectChanges();
+        },
+        (error) => {
+          this.isProceess = false;
+        }
+      );
+    }
+    else{
+      this.slaData = [];
+    }
   }
   getPriority() {
-    this.masterName = `/servicetitle/servicePriority/${this.categoryid.serviceTitle?.serviceId}`
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.slaData = data;
-      this.selectedOption = this.slaData[0].defaultPriority.id;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+    this.masterName = `/servicetitle/servicePriority/${this.categoryid.serviceTitle?.serviceId}`;
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.slaData = data;
+        this.selectedOption = this.slaData[0].defaultPriority.id;
+        this.isProceess = false;
+        this.cd.detectChanges();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
   onFileChange(event: any) {
     let data;
@@ -192,109 +216,124 @@ export class AddEditeTicketComponent {
       data = event.target.files[0];
     if (event.target.files[0].name && event.target.files.length > 0) {
       data = event.target.files[0];
-    }
-    else {
-      data = "null"
+    } else {
+      data = 'null';
     }
     this.uploadFile = data;
   }
   ActiveUser() {
-    this.masterName = "/users/active"
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.aciveUser = data;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+    this.masterName = '/users/active';
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.aciveUser = data;
+        this.isProceess = false;
+        this.cd.detectChanges();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
   getDepartment() {
-    this.masterName = "/department/active"
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.dept = data;
-      this.selectedOption1 = this.userData?.department?.departmentId;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+    this.masterName = '/department/active';
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.dept = data;
+        this.selectedOption1 = this.userData?.department?.departmentId;
+        this.isProceess = false;
+        this.cd.detectChanges();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
   selectDepartment() {
     this.masterName = `/users/active/${this.userData?.department?.departmentId}`;
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.duser = data;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.duser = data;
+        this.isProceess = false;
+        this.cd.detectChanges();
+        this.selectDepartment();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
   selectDEPT() {
     this.masterName = `/users/active/${this.categoryid.department?.departmentId}`;
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.duser = data;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.duser = data;
+        this.isProceess = false;
+        this.cd.detectChanges();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
   subCategory() {
-    this.masterName = `/subcategory/active/${this.categoryid.category?.categoryId}`
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.sdata = data;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+    this.masterName = `/subcategory/active/${this.categoryid.category?.categoryId}`;
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.sdata = data;
+        this.isProceess = false;
+        this.cd.detectChanges();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
   changed(e: any) {
-    if(e !==''){
-      this.masterName = `/subcategory/active/${e}`
-      this.apiService.getAll(this.masterName).subscribe(data => {
-        this.sdata = data;
-        this.selectDepartment();
-        this.isProceess = false;
-        this.cd.detectChanges();
-      }, error => {
-        this.isProceess = false;
-      });
-    }
-    else{
+    if (e !== '') {
+      this.masterName = `/subcategory/active/${e}`;
+      this.apiService.getAll(this.masterName).subscribe(
+        (data) => {
+          this.sdata = data;
+          this.isProceess = false;
+          this.cd.detectChanges();
+        },
+        (error) => {
+          this.isProceess = false;
+        }
+      );
+    } else {
       this.sdata = [];
     }
-
-
   }
   selectCategory(e: any) {
-    if(e !==''){
-      this.masterName = `/servicetitle/active/${e}`
-      this.apiService.getAll(this.masterName).subscribe(data => {
-        this.data1 = data;
-
-        this.isProceess = false;
-        this.cd.detectChanges();
-      }, error => {
-        this.isProceess = false;
-      });
-    }
-    else{
+    if (e !== '') {
+      this.masterName = `/servicetitle/active/${e}`;
+      this.apiService.getAll(this.masterName).subscribe(
+        (data) => {
+          this.data1 = data;
+          this.isProceess = false;
+          this.cd.detectChanges();
+        },
+        (error) => {
+          this.isProceess = false;
+        }
+      );
+    } else {
       this.data1 = [];
     }
-
   }
   getserviceTitle() {
-
     this.masterName = `/servicetitle/active/${this.categoryid.subCategory?.subCategoryId}`;
-
-    this.apiService.getAll(this.masterName).subscribe(data => {
-      this.data1 = data;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    }, error => {
-      this.isProceess = false;
-    });
+    this.apiService.getAll(this.masterName).subscribe(
+      (data) => {
+        this.data1 = data;
+        this.isProceess = false;
+        this.cd.detectChanges();
+      },
+      (error) => {
+        this.isProceess = false;
+      }
+    );
   }
   onCancel() {
     this.activeModal.dismiss();
@@ -315,8 +354,8 @@ export class AddEditeTicketComponent {
         createForUser: this.ticketMasterForm.value.createForUser,
         status: this.ticketMasterForm.value.status,
         file: this.uploadFile || null,
-      }
-      this.activeModal.close(data)
+      };
+      this.activeModal.close(data);
     } else {
       this.ticketMasterForm.controls['subCategory'].markAsTouched();
       this.ticketMasterForm.controls['category'].markAsTouched();
@@ -333,6 +372,9 @@ export class AddEditeTicketComponent {
     }
   }
   shouldShowError(controlName: string, errorName: string) {
-    return this.ticketMasterForm.controls[controlName].touched && this.ticketMasterForm.controls[controlName].hasError(errorName);
+    return (
+      this.ticketMasterForm.controls[controlName].touched &&
+      this.ticketMasterForm.controls[controlName].hasError(errorName)
+    );
   }
 }
