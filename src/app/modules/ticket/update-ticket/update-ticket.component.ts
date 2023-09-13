74191a3c0d27e7d824562fa3d8cf9b8e070d68fe
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2023-09-13 12:17:47 +0530

    update ticket

--- src/app/modules/ticket/update-ticket/update-ticket.component.ts
+++ src/app/modules/ticket/update-ticket/update-ticket.component.ts
@@ -146,12 +146,18 @@ export class UpdateTicketComponent {
   }
 
   selectCategory(e: any) {
-    this.masterName = `/servicetitle/active/${e}`;
-    this.apiService.getAll(this.masterName).subscribe((data) => {
-      this.data1 = data;
-      this.isProceess = false;
-      this.cd.detectChanges();
-    });
+    if(e !==''){
+      this.masterName = `/servicetitle/active/${e}`;
+      this.apiService.getAll(this.masterName).subscribe((data) => {
+        this.data1 = data;
+        this.isProceess = false;
+        this.cd.detectChanges();
+      });
+    }
+    else{
+      this.data1 = [];
+    }
+
   }
 
 
