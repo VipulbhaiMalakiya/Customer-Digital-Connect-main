56fe94f1478665c94cdc5dcc966f856ce01f340c
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2023-09-22 08:54:40 +0530

    v

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   apiUrl: 'https://customerdigitalconnect.com',
   EncryptKey:"1203199320052021",
   netCoreUrl:"https://waapi.pepipost.com/api/v2/",
-  SOCKET_ENDPOINT: 'wss://customerdigitalconnect.com/chat'
+  SOCKET_ENDPOINT: 'wss://customerdigitalconnect.com/chat',
+  googleMapsApiKey: 'YOUR_API_KEY',
 };
