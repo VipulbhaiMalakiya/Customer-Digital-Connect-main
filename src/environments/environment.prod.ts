6d3c7030fdca9e3dc2b5d0b95f4fa4e08ac83daf
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-01-06 13:25:47 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,10 @@
 export const environment = {
   production: false,
   //prod
-  apiUrl: 'https://customerdigitalconnect.com',
-  // apiUrl: 'http://35.154.46.164:8080',
+  // apiUrl: 'https://customerdigitalconnect.com',
+  apiUrl: 'http://65.1.109.24:9091', //Uat
+
   EncryptKey:"1203199320052021",
-  // netCoreUrl:"https://waapi.pepipost.com/api/v2/",
   SOCKET_ENDPOINT: 'wss://customerdigitalconnect.com/chat',
-  // SOCKET_ENDPOINT: 'ws://35.154.46.164:8080/chat',
-  // deepcode ignore HardcodedNonCryptoSecret: <please specify a reason of ignoring this>
   googleMapsApiKey: 'YOUR_API_KEY',
 };
