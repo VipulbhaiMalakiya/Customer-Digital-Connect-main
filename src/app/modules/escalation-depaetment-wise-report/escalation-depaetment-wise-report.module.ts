87936acdba3492311eedbf26fcdd26da1735c6ec
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-02-27 18:41:21 +0530

    update

--- src/app/modules/escalation-depaetment-wise-report/escalation-depaetment-wise-report.module.ts
+++ src/app/modules/escalation-depaetment-wise-report/escalation-depaetment-wise-report.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { EscalationDepaetmentWiseReportRoutingModule } from './escalation-depaetment-wise-report-routing.module';
 import { EscalationDepaetmentWiseReportComponent } from './escalation-depaetment-wise-report.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { SharedModule } from '../shared/shared.module';
+import { eeFilterPipe } from 'src/app/_helpers/EscalationDepaetment-filter';
 
 
 @NgModule({
   declarations: [
-    EscalationDepaetmentWiseReportComponent
+    EscalationDepaetmentWiseReportComponent,
+    eeFilterPipe
   ],
   imports: [
     CommonModule,
-    EscalationDepaetmentWiseReportRoutingModule
+    EscalationDepaetmentWiseReportRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule
   ]
 })
 export class EscalationDepaetmentWiseReportModule { }
