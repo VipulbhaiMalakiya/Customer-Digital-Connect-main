f515a6ee75af5b7838bf7539c2e701e048ef7660
Vipulbhaimalakiya <vipul.malakiya@teamcomputers.com.com>
2024-03-07 12:49:37 +0530

    update

--- src/app/modules/escallation-level/escallation-level.component.ts
+++ src/app/modules/escallation-level/escallation-level.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription, take } from 'rxjs';
+import { ApiService } from 'src/app/_api/rxjs/api.service';
+import { slaMasterModel } from 'src/app/_models/sla';
+import { AppService } from 'src/app/_services/app.service';
+import { ViewSlaComponent } from '../sla/components/view-sla/view-sla.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-escallation-level',
@@ -7,4 +16,159 @@ import { Component } from '@angular/core';
 })
 export class EscallationLevelComponent {
 
-}
+    isProceess: boolean = true;
+    term: any;
+    data: any[] = [];
+    subscription?: Subscription;
+    userData: any;
+    masterName?: any;
+    page: number = 1;
+    count: number = 0;
+    tableSize: number = 10;
+    tableSizes: any = [3, 6, 9, 12];
+  
+    constructor(
+      private cd: ChangeDetectorRef,
+      private modalService: NgbModal,
+      private toastr: ToastrService,
+      private titleService: Title,
+      private appService: AppService,
+      private apiService: ApiService,
+    ) {
+      this.titleService.setTitle("CDC -Escalation Level");
+      const d: any = localStorage.getItem("userData");
+      this.userData = JSON.parse(d);
+    }
+  
+    ngOnInit(): void {
+      this.fatchData();
+    }
+  
+    fatchData() {
+      this.isProceess = true;
+      this.masterName = "/ticket/sla";
+      this.subscription = this.apiService.getAll(this.masterName).pipe(take(1)).subscribe(data => {
+        if (data) {
+          this.data = data.data;
+          this.count = this.data.length;
+          this.isProceess = false;
+          this.cd.detectChanges();
+        }
+  
+      }, error => {
+        this.isProceess = false;
+      })
+    }
+    onTableDataChange(event: any) {
+      this.page = event;
+    }
+    onTableSizeChange(event: any): void {
+      this.tableSize = event.target.value;
+      this.page = 1;
+    }
+    onDownload() {
+      const exportData = this.data.map(x => {
+        let updatedBy: any = ' '
+        if (x.updatedBy?.firstName != undefined) {
+          updatedBy = x.updatedBy?.firstName + ' ' + x.updatedBy?.lastName
+        }
+        else {
+          updatedBy = ''
+        }
+  
+        let assignedTo: any = ' '
+        if (x.assignedTo?.firstName != undefined) {
+          assignedTo = x.assignedTo?.firstName || '' + ' '  + x.assignedTo?.lastName || ''
+        }
+        else {
+          assignedTo = ''
+        }
+  
+        let resolveBy: any = ' '
+        if (x.resolveBy?.firstName != undefined) {
+          resolveBy =x.resolveBy?.firstName || '' + ' '  + x.resolveBy?.lastName || ''
+        }
+        else {
+          resolveBy = ''
+        }
+  
+        let createForUser: any = ' '
+        if (x.createForUser?.firstName != undefined) {
+          createForUser =x.createForUser?.firstName || '' + ' '  + x.createForUser?.lastName || ''
+        }
+        else {
+          createForUser = ''
+        }
+  
+        let createForCustomer: any = ' '
+        if (x.createForUser?.firstName != undefined) {
+          createForCustomer =x.createForCustomer?.firstName || '' + ' '  + x.createForCustomer?.lastName || ''
+        }
+        else {
+          createForCustomer = ''
+        }
+  
+        let updatedByCustomer: any = ' '
+        if (x.updatedByCustomer?.firstName != undefined) {
+          createForCustomer =x.updatedByCustomer?.firstName || '' + ' '  + x.updatedByCustomer?.lastName || ''
+        }
+        else {
+          updatedByCustomer = ''
+        }
+  
+        return {
+          "Id": x.ticketId || '',
+          "ticketNo": x.ticketNo || '',
+          "emailId": x.emailId || '',
+          "category":x.category?.categoryName || '',
+          "subCategory":x.subCategory?.subCategoryName || '',
+          "serviceTitle":x.serviceTitle?.serviceName ||'' ,
+          "department":x.department?.departmentName || '',
+          "alternativeContactNo":x.alternativeContactNo || '',
+          "priority":x.priority || '',
+          "issue":x.issue?.issueName || '',
+          "assignedTo":assignedTo,
+          "resolveBy":resolveBy,
+          "createForUser":createForUser,
+          "createForCustomer":createForCustomer,
+          "updatedByCustomer":updatedByCustomer,
+          "mode":x.mode || '',
+          "resolveTimeStamp":x.resolveTimeStamp || '',
+          "sla":x.sla,
+          "ticketStatus":x.ticketStatus || '',
+          "Created By": x.createdBy?.firstName + ' ' + x.createdBy?.lastName || '',
+          "Created Date": moment(x.createdDate || '').format("llll"),
+          "Updated By": updatedBy,
+          "Updated Date": moment(x.updatedDate || '').format("llll"),
+          "Status": x.status ? 'Active' : 'Deactivate'
+        }
+      });
+      const headers = [
+        "Id", "ticketNo", "emailId","category","subCategory","serviceTitle",
+        "department","alternativeContactNo","priority","issue","assignedTo",
+        "resolveBy","createForUser","createForCustomer","updatedByCustomer",
+        "mode","resolveTimeStamp","sla","ticketStatus",
+        "Created By", "Created Date", "Updated By", "Updated Date", "Status"
+      ];
+      this.appService.exportAsExcelFile(exportData, "sla-Report", headers);
+    }
+  
+  
+  
+    ngOnDestroy(): void {
+      this.subscription?.unsubscribe();
+    }
+  
+    onViewDetail(dataItem: slaMasterModel) {
+      this.isProceess = true;
+      const modalRef = this.modalService.open(ViewSlaComponent, { size: "xl", centered: true, backdrop: "static" });
+      if (modalRef) {
+        this.isProceess = false;
+      } else {
+        this.isProceess = false;
+      }
+      var componentInstance = modalRef.componentInstance as ViewSlaComponent;
+      componentInstance.serviceTitleMaster = dataItem;
+    }
+  }
+  
--- src/app/modules/escallation-level/escallation-level.module.ts
+++ src/app/modules/escallation-level/escallation-level.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { EscallationLevelRoutingModule } from './escallation-level-routing.module';
 import { EscallationLevelComponent } from './escallation-level.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { SharedModule } from '../shared/shared.module';
+import { eipipe } from 'src/app/_helpers/Escalation-Level.filter';
 
 
 @NgModule({
   declarations: [
-    EscallationLevelComponent
+    EscallationLevelComponent,
+    eipipe
   ],
   imports: [
     CommonModule,
-    EscallationLevelRoutingModule
+    EscallationLevelRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    FormsModule
   ]
 })
 export class EscallationLevelModule { }
